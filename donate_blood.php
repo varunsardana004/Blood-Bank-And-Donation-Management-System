f46aae26ec77a5f61c0ad56b77bca94750a339ae
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-05 12:18:24 +0530

    added donate.php via upload
    page where you give your name to donate blood

---
+++ donate_blood.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<?php include('head.php') ?>
+
+<div class="container" style="margin-top:30px">
+
+<div class="row">
+    <div class="col-lg-6">
+        <h1 class="mt-4 mb-3">Donate Blood </h1>
+      </div>
+</div>
+
+<form name="donor" action="savedata.php" method="post">
+<div class="row">
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Full Name<span style="color:red">*</span></div>
+<div><input type="text" name="fullname" class="form-control" required></div>
+</div>
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Mobile Number<span style="color:red">*</span></div>
+<div><input type="text" name="mobileno" class="form-control" required></div>
+</div>
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Email Id</div>
+<div><input type="email" name="emailid" class="form-control"></div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Age<span style="color:red">*</span></div>
+<div><input type="text" name="age" class="form-control" required></div>
+</div>
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Gender<span style="color:red">*</span></div>
+<div><select name="gender" class="form-control" required>
+<option value="">Select</option>
+<option value="Male">Male</option>
+<option value="Female">Female</option>
+</select>
+</div>
+</div>
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Blood Group<span style="color:red">*</span></div>
+<div><select name="blood" class="form-control" required>
+  <option value=""selected disabled>Select</option>
+  <?php
+    include 'conn.php';
+    $sql= "select * from blood";
+    $result=mysqli_query($conn,$sql) or die("query unsuccessful.");
+  while($row=mysqli_fetch_assoc($result)){
+   ?>
+   <option value=" <?php echo $row['blood_id'] ?>"> <?php echo $row['blood_group'] ?> </option>
+  <?php } ?>
+</select>
+</div>
+</div>
+
+</div>
+<div class="row">
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Address<span style="color:red">*</span></div>
+<div><textarea class="form-control" name="address" required></textarea></div></div>
+
+</div>
+<div class="row">
+  <div class="col-lg-4 mb-4">
+  <div><input type="submit" name="submit" class="btn btn-primary" value="Submit" style="cursor:pointer"></div>
+  </div>
+</div>
+
+</div>
+
+
+
+
+<?php include('footer.php') ?>
+</body>
+</html>
