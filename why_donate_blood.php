e43a035ad20331ecb1bf7eee14e39219c3573d12
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-05 11:47:10 +0530

    why-donate_blood.php
    Page about why to donate blood

---
+++ why_donate_blood.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+
+<?php include('head.php');
+?>
+
+
+<div class="container" style="margin-top:30px">
+
+<div class="row">
+    <div class="col-lg-6">
+        <h1 class="mt-4 mb-3">Why Should I Donate Blood ? </h1>
+        <p>
+          <?php
+            include 'conn.php';
+            $sql=$sql= "select * from pages where page_type='donor'";
+            $result=mysqli_query($conn,$sql);
+            if(mysqli_num_rows($result)>0)   {
+                while($row = mysqli_fetch_assoc($result)) {
+                  echo $row['page_data'];
+                }
+              }
+
+           ?>
+      </p>
+
+    </div>
+    <div class="col-lg-6">
+        <img class="img-fluid rounded" src="image\08f2fccc45d2564f74ead4a6d5086871.png" style="height:600px; width:500px" alt="error"  >
+    </div>
+</div>
+</div>
+
+
+
+<?php include('footer.php')
+?>
+</body>
+
+</html>
