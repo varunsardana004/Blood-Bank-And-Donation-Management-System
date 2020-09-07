cb6f75576977a771f4fe6daa6165df573f709f94
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-07 20:01:37 +0530

    Add files via upload

---
+++ slider.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+  <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container" style="margin-top:100px">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+      </ol>
+
+      <div class="carousel-inner">
+        <div class="item active">
+            <img src="image\_107317099_blooddonor976.jpg" alt="Los Angeles" style="width:100%; height:550px;margin-top:0px;">
+        </div>
+
+        <div class="item">
+          <img src="image\Blood-facts_10-illustration-graphics__canteen.png" alt="Chicago" style="width:100%;height:550px;margin-top:0px;">
+        </div>
+      </div>
+
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+</div>
+</body>
+
+
+</html>
