0b2664b42285b753a7fe29ec4fb70db2513c9f00
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-07 14:16:13 +0530

    Add files via upload

---
+++ savedata.php
@@ -0,0 +1,16 @@
+<?php
+$name=$_POST['fullname'];
+$number=$_POST['mobileno'];
+$email=$_POST['emailid'];
+$age=$_POST['age'];
+$gender=$_POST['gender'];
+$blood_group=$_POST['blood'];
+$address=$_POST['address'];
+$conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
+$sql= "INSERT INTO donor_details(donor_name,donor_number,donor_mail,donor_age,donor_gender,donor_blood,donor_address) values('{$name}','{$number}','{$email}','{$age}','{$gender}','{$blood_group}','{$address}')";
+$result=mysqli_query($conn,$sql) or die("query unsuccessful.");
+
+header("Location: http://localhost/BDMS/home.php");
+
+mysqli_close($conn);
+ ?>
