aa5f3616662ecf12338b0844a5b50d88255daa3f
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-16 10:31:24 +0530

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -18,9 +18,9 @@
 <?php include('head.php');
 ?>
 
-
-<div class="container" style="margin-top:50px">
-
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+  <div class="container">
+  <div id="content-wrap" style="padding-bottom:50px;">
 <div class="row">
     <div class="col-lg-6">
         <h1 class="mt-4 mb-3">About Us</h1>
@@ -42,10 +42,11 @@
         <img class="img-fluid rounded" src="image\banner_590x300.jpg" style="height:400px" alt="error"  >
     </div>
 </div>
-</div>
-<br><br>
+</div></div>
+
 <?php include('footer.php')
-?> 
+?>
+</div>
 </body>
 
 </html>
