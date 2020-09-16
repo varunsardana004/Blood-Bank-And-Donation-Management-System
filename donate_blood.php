b7983e401063ad3888b654bcf6a2a3ed0af87eab
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-16 10:33:11 +0530

    Update donate_blood.php

--- donate_blood.php
+++ donate_blood.php
@@ -15,7 +15,9 @@
 
 <?php include('head.php') ?>
 
-<div class="container" style="margin-top:30px">
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+  <div class="container">
+  <div id="content-wrap" style="padding-bottom:50px;">
 
 <div class="row">
     <div class="col-lg-6">
@@ -82,10 +84,11 @@
 </div>
 
 </div>
-
+</div>
 
 
 
 <?php include('footer.php') ?>
+</div>
 </body>
 </html>
