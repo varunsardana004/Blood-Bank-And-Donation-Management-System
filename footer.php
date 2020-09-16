5bd440b338c4b22997290162928327931b67ec8b
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-16 10:37:45 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,23 @@
 <html>
 <head>
   <style>
+  #footer {
 
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
   </style>
 </head>
 <body>
-  <div id="footer" style="background-color:#000000;height:75px;width:100%;color:white" >
-  <b><center>COPYRIGHT 2020<br>
-  Blood Bank & Donation
+  <div id="footer" >
+  <b><center>COPYRIGHT © 2020<br>
+  Blood Bank & Donation Management
   <br>
   ALL RIGHTS RESERVED.
   </center>
