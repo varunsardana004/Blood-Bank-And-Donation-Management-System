8f1e2eea192a949f19ee8528adf1e814a53616fd
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-10-17 00:51:40 +0530

    Update head.php
    added active class to header links

--- head.php
+++ head.php
@@ -24,6 +24,7 @@
   font-size: 18px;
   line-height: 25px;
   border-radius: 4px;
+  font-weight:bold;
 }
 
 /* Style the logo link (notice that we set the same value of line-height and font-size to prevent the header to increase when the font gets bigger */
@@ -39,7 +40,7 @@
   color: black;
 }
 
-/* Style the active/current link*/
+
 
 
 /* Float the link section to the right */
@@ -58,17 +59,21 @@
     float: none;
   }
 }
+/* Style the active/current link*/
 a.act{
 background: linear-gradient(to right, #fd746c 0%, #ff9068 100%);
 color: white;
 border-radius:30px;
+}
+a.logo2{
+  background-color: #333;
 }
 </style>
 </head>
 
 <body>
   <div class="header">
-    <a href="home.php" class="logo">Blood Bank & Donation </a>
+    <a href="home.php" class="logo"<?php if($active=='home') echo "class='logo2'"; ?>>Blood Bank & Donation </a>
     <div class="header-right">
     <a href="about_us.php"  <?php if($active=='about') echo "class='act'"; ?> >About Us</a>
     <a href="why_donate_blood.php"  <?php if($active=='why') echo "class='act'"; ?>>Why Donate Blood</a>
